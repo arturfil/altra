85032da4ec3f1392d2ce9590a06a9392b03b5db0
Arturo Filio Villa <arturfil@hotmail.com>
2018-10-26 17:04:21 -0400

    styling changes

--- altra_services_client/src/components/landing/Landing.jsx
+++ altra_services_client/src/components/landing/Landing.jsx
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
-import Carousel from '../../components/carousel/Carousel'
+import Carousel from '../../components/carousel/Carousel';
 
 import './Landing.css';
-import icon from '../../img/2friends_icon.jpg';
 
 class Landing extends Component {
   render() {
     return (
       <div>
         <div className="landing">
-            
-                  <Carousel/>
-            
+          <Carousel />
         </div>
       </div>
-    )
+    );
   }
 }
 
