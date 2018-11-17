b0be987eae1a2b9aea8dca0391b4316d67a1a5ad
Arturo Filio Villa <arturfil@hotmail.com>
2018-11-16 22:22:02 -0500

    added some styling to our services page

--- altra_services_client/src/components/contact-us/ContactUs.jsx
+++ altra_services_client/src/components/contact-us/ContactUs.jsx
@@ -45,11 +45,21 @@ class ContactUs extends Component {
     });
   };
 
-  onClick = () => {
-    this.setState({ showMenu: true });
-    setTimeout(() => {
-      this.setState({ showMenu: false });
-    }, 3000);
+  onClick = (e) => {
+    if(this.state.name !== "" && 
+       this.state.email !== "" && 
+       this.state.phone !== "") {
+
+      this.setState({ showMenu: true });
+      setTimeout(() => {
+        this.setState({ showMenu: false });
+      }, 3000);
+
+      this.setState({
+        name: ""
+      })
+    }
+
   };
 
   render() {
@@ -57,15 +67,44 @@ class ContactUs extends Component {
         <h2 className="text-center title">Lets work together!</h2>
         <hr className="basic" />
         <form onSubmit={this.onSubmit} className="text-center form-group">
-          <input name="name" className="form-size" type="text" placeholder="Name" onChange={this.onChange} />
+          <input 
+            name="name" 
+            className="form-size" 
+            type="text" 
+            placeholder="Name" 
+            onChange={this.onChange} 
+            required />
           <br />
-          <input name="email" className="form-size" type="text" placeholder="Email" onChange={this.onChange} />
+          <input 
+            name="email" 
+            className="form-size" 
+            type="text" 
+            placeholder="Email" 
+            onChange={this.onChange} 
+            required />
           <br />
-          <input name="company" className="form-size" type="text" placeholder="Company" onChange={this.onChange} />
+          <input 
+            name="company" 
+            className="form-size" 
+            type="text" 
+            placeholder="Company" 
+            onChange={this.onChange} />
           <br />
-          <input name="phone" className="form-size" type="text" placeholder="Phone" onChange={this.onChange} />
+          <input 
+            name="phone" 
+            className="form-size" 
+            type="text" 
+            placeholder="Phone" 
+            onChange={this.onChange} 
+            required />
           <br />
-          <textarea name="message" className="form-area" col="8" type="text" placeholder="Write your message here ..." onChange={this.onChange} />
+          <textarea 
+            name="message" 
+            className="form-area" 
+            col="8" 
+            type="text" 
+            placeholder="Write your message here ..." 
+            onChange={this.onChange} />
           <br />
           {this.state.showMenu ? <div className="container">
               <div className="alert alert-success alert-box" role="alert">
